ff03ed4c04bb54f3d9953e1b3f257be3fb3d1da8
briannabek2009 <briannab364@nycstudents.net>
2026-01-08 14:08:25 +0000

    update

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,21 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function cat1(){
+    document.getElementById("highlight").innerHTML=
+    "cute orange cat"
+}
+function cat2(){
+    document.getElementById("highlight").innerHTML=
+    "cute black cat"
+}
+function cat3(){
+    document.getElementById("highlight").innerHTML=
+    "cute gray cat"
+}
+function cat4(){
+    document.getElementById("highlight").innerHTML=
+    "cute brown cat"
+}
+function cat5(){
+    document.getElementById("highlight").innerHTML=
+    "cute orange cat"
+}
--- WDLesson3_4Practical/script.js
+++ WDLesson3_4Practical/script.js
@@ -17,9 +17,16 @@ function createStory(){
   Choose your variable names wisely.  This is a lot of practice of creating variables and using document.getElementById(..)
   */
 
+    let boyname=document.getElemntById("boyname")
+let vegetable=document.getElemntById("vegetable")
+let girlname=document.getElemntById("girlname")
+let anothervegetable=document.getElemntById("anothervegetable")
   /* Task 4: Set the innerHTML for each of the spans on lines 3 to 9 with the appropriate values from the variables you create in Task 3. */
-
+boy1span1.innerHTML = boyname.value;
+boy1span2.innerHTML = boyname.value;
+boy1span3.innerHTML = boyname.value;
+boy1span4.innerHTML = boyname.value;
   /* Task 5: Set the display property of the element with an id of "story" to 'block' */
-
+document.getElementById("story").style.display="block";
 
 }
