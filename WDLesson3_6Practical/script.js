d54d49819839e7d70c1f4c5effff627c30a3e5ce
briannabek2009 <briannab364@nycstudents.net>
2026-01-16 14:30:40 +0000

    update

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,7 +1,27 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
+function average(){
+    let g1 = parseFloat(document.getElementById("g1").value);
+     let g2 = parseFloat(document.getElementById("g2").value);
+      let g3 = parseFloat(document.getElementById("g3").value);
+      let output = document.getElementById("output_average");
 
+      let avg = (g1 + g2 + g3)/
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
+ function Slope(){
+let x1 = parseFloat(document.getElementById("x1").value);
+     let y1 = parseFloat(document.getElementById("y1").value);
+      let x2 = parseFloat(document.getElementById("x2").value);
+         let y2 = parseFloat(document.getElementById("y2").value);
+      let output = document.getElementById("output_Slope");
 
+    
+ }
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+ function BMI (){
+let x1 = parseFloat(document.getElementById("x1").value);
+     let y1 = parseFloat(document.getElementById("y1").value);
+      
+      let output = document.getElementById("output_Slope");
