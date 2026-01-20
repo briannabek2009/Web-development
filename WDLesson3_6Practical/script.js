829df141058a98c6f71510ffd395d4c3a8356469
briannabek2009 <briannab364@nycstudents.net>
2026-01-20 14:34:08 +0000

    update

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -5,23 +5,25 @@ function average(){
       let g3 = parseFloat(document.getElementById("g3").value);
       let output = document.getElementById("output_average");
 
-      let avg = (g1 + g2 + g3)/
+average = (g1 + g2 + g3)/3;
+output.innerHTML= "average is" + average
 }
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
  function Slope(){
-let x1 = parseFloat(document.getElementById("x1").value);
-     let y1 = parseFloat(document.getElementById("y1").value);
-      let x2 = parseFloat(document.getElementById("x2").value);
-         let y2 = parseFloat(document.getElementById("y2").value);
-      let output = document.getElementById("output_Slope");
-
-    
+          let x1 = parseFloat(document.getElementById("x1").value);
+          let y1 = parseFloat(document.getElementById("y1").value);
+          let x2 = parseFloat(document.getElementById("x2").value);
+          let y2 = parseFloat(document.getElementById("y2").value);
+          let output = document.getElementById("output_Slope");
+          let m=(y1-y2)/(x1-x2)
+        
+output.innerHTML= "Slope is" + Slope
  }
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
- function BMI (){
-let ht= parseFloat(document.getElementById("ht").value);
-     let wt = parseFloat(document.getElementById("wt").value);
-      
-      let output = document.getElementById("output_BMI");
+      function BMI (){
+         let ht= parseFloat(document.getElementById("ht").value);
+         let wt = parseFloat(document.getElementById("wt").value);
+         let output = document.getElementById("output_BMI");
+      }
