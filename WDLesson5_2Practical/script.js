a1ebf8f962928c323fa9cd7687a83993a379eade
Brianna <briannab364@nycstudents.net>
2026-03-06 14:34:37 +0000

    update

--- WDLesson5_2Practical/script.js
+++ WDLesson5_2Practical/script.js
@@ -16,7 +16,22 @@ Now that you have the template, use a for loop to produce all five pizza cards.
 
 //init() function is called when the page loads in index.html
 function init(){
-  
+    let output=""
 
-}
+     let bulid =""
+    for( let i = 0; i<10; i+= 1)}
+    build += ` <div id="output" class="container center">`
+    build += ` <img src="logo.png" >  `
+    build += `<img src =" pizza1.jpg" div class = "pizza">`
+    build += `< img src = " title1. png div class="title"> `
+    build += `<img src =" pizza2.jpg" div class = "pizza">`
+    build += `< img src = " title2. png div class="title"> `
+    build += `<img src =" pizza3.jpg" div class = "pizza">`
+    build += `< img src = " title3. png div class="title"> `
+    build += `<img src =" pizza4.jpg" div class = "pizza">`
+    build += `< img src = " title4. png div class="title"> `
+    build += `<img src =" pizza5.jpg" div class = "pizza">`
+        build += `< img src = " title5. png div class="title"> `
+
+
 
