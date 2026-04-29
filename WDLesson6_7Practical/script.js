5b15b42e1112142ef5114d7d8431ef972ab7eabd
Brianna <briannab364@nycstudents.net>
2026-04-29 13:33:59 +0000

    update

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -1,17 +1,24 @@
 let data;
+async function init(){
 
-async function init()
-
-let link="collision.json"
+let link="collision.json";
 info = await fetch(link);
 data = await info.json();
 
-let output = documents.getElementsById("output");
+let output = document.getElementById("output");
 let build ="";
 
 for(let i = 0; i < data.length; i+=1){
     let collision = data[i];
-    build += `<div class="card">`
+    build += `<div class="card">
           
-    <h2> ${collision.name}</h2>
+    build+=`<h2> ${collision.name}</h2>;
+    <p> ${collision.crash_date}</p>
+    <p> ${collision.crash_time}</p>
+                 
     
+    </div>
+    `
+}
+output.innerHTML= build;
+}
